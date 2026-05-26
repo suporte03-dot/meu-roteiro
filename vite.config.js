2814e779d17e6648df84968125f4f0975fa1dc1f
Marlon Derlam <suporte03@sollucao.net>
2026-05-26 16:47:47 -0300

    Configura base do Vite para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/meu-roteiro/',
 })
+npm.cmd run build
+npm.cmd install --save-dev gh-pages
+npm.cmd run deploy
+
